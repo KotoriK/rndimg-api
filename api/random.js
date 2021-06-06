d3c558c634774fe75cf77094dc199453a0c6f111
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-06-06 16:54:17 +0800

    fix

--- api/random.js
+++ api/random.js
@@ -6,6 +6,6 @@ module.exports =
  * @param {import('@vercel/node').VercelResponse} res 
  */
 (req, res) => {
-    const randomIndex = Math.random() * (range[1] - range[0] + 1) - range[0]
+    const randomIndex = Math.floor(Math.random() * (range[1] - range[0] + 1) - range[0])
     res.redirect(302,url+randomIndex+'.webp')
 }
